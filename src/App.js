d17e26ceead7527eae77f471739b41d42b52bc5f
VaibhavRanghar <vaibhavranghar150@gmail.com>
2024-02-25 13:49:28 +0530

    Custom dashboard of discord project setup with sidebar component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import SideBar from "./components/SideBar";
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
